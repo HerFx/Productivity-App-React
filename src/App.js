cec7412445c6a6cd9657642ebde90d35ea550aa3
Mikołaj Janiak <janiak.m2222@gmail.com>
2023-04-15 13:12:43 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import ToDo from './components/pages/ToDo';
